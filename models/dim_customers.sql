06493eaeded93c5424494e5754e2178f1548916c
mohan@eoncollective.com <mohan@eoncollective.com>
2021-10-26 21:49:31 +0000

    success

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -4,6 +4,21 @@
 
 with customers as (
 
+    select * from {{ ref('stg_customers')}}
+
+),
+
+orders as (
+
+    select * from {{ ref('stg_orders') }}
+
+),
+
+
+
+
+with customers as (
+
     select
         id as customer_id,
         first_name,
@@ -13,17 +28,7 @@ with customers as (
 
 ),
 
-orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from DEMO_DB.jaffle_shop.orders
-
-),
 
 
 
