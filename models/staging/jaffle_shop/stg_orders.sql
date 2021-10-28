76abb519bf73b21b8a177162d928c9aa18db6a7c
mohan@eoncollective.com <mohan@eoncollective.com>
2021-10-28 13:14:26 +0000

    tsest completed

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,13 +1,7 @@
-with orders as (
-
-    select
+ select
         id as order_id,
         user_id as customer_id,
         order_date,
         status
 
-    from DEMO_DB.jaffle_shop.orders
-
-),
-
-select * from orders
+    from demo_db.jaffle_shop.orders
