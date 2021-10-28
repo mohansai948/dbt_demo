036078f26cb3d87153b3037662abdbdf4f6bc436
mohan@eoncollective.com <mohan@eoncollective.com>
2021-10-28 12:44:06 +0000

    jaffle_shop_customers_success

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,12 +1,6 @@
-with customers as (
-
-    select
+select
         id as customer_id,
         first_name,
         last_name
 
     from DEMO_DB.jaffle_shop.customers
-
-),
-
-select * from customers
